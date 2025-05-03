f59d5d5f2d62a51211ac41a079aa44de5917159a
mudiagaobriki <mudi_invents@yahoo.com>
2025-05-03 21:48:15 +0100

    Predeployment 3

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import uvicorn
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-import pyIGRF
+# import pyIGRF
 
 app = FastAPI()
 
